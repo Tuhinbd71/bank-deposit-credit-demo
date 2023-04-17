d61069a08823c2f924c7a243e6684c6d647d9b52
Tuhinbd71 <m.tuhin02bd@gmail.com>
2023-04-18 02:52:37 +0600

    new commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// user login Form 
 document.getElementById("submit-btn").addEventListener("click", function () {
     // get user email
     const inputField = document.getElementById("user-email");
@@ -6,7 +7,7 @@ document.getElementById("submit-btn").addEventListener("click", function () {
     const userPassword = document.getElementById("user-password");
     const userPasswordValue = userPassword.value;
     if (userEmail == "m.tuhin02bd@gmail.com" && userPasswordValue == "THN@00dotcom") {
-        console.log("Congratulation!");
+        window.location.href = "banking.html"
     }
     else {
         console.log("Please! input valid value")
