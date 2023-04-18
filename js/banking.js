deeefde41d319f9bd7c7260c6446024f809b6493
Tuhinbd71 <m.tuhin02bd@gmail.com>
2023-04-18 19:57:52 +0600

    new commit

--- js/banking.js
+++ js/banking.js
@@ -1,6 +1,14 @@
 // User deposit & withdraw section
 document.getElementById("deposit-btn").addEventListener("click", function () {
+    // get the amount deposited
     const depositInput = document.getElementById("deposit-input");
-    const depositValue = depositInput.value;
-    console.log(depositValue);
+    const newDepositAmount = depositInput.value;
+    depositInput.value = "";
+    // console.log(depositAmount);
+
+    const depositField = document.getElementById("deposit-add");
+    const previousDepositAmount = depositField.innerText;
+    const newDdepositTotal = parseFloat(previousDepositAmount) + parseFloat(newDepositAmount);
+    depositField.innerText = newDdepositTotal;
+
 })
