b47cd297063a9cdbdcc26ecc67dac9586c3d2ec6
Tuhinbd71 <m.tuhin02bd@gmail.com>
2023-04-19 04:32:57 +0600

    new commit

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,7 +1,7 @@
+// handle withdraw event handler
 document.getElementById("withdraw-btn").addEventListener("click", function () {
     const withdrawField = document.getElementById("withdraw-input");
     const withdrawFieldValue = withdrawField.value;
-
     const withdrawFieldAmount = parseFloat(withdrawFieldValue);
     withdrawField.value = withdrawFieldAmount;
 
@@ -10,7 +10,14 @@ document.getElementById("withdraw-btn").addEventListener("click", function () {
     const withdrawBalanceValue = parseFloat(withdrawBalanceTextValue);
     const withdrawBalanceTotalAmount = withdrawBalanceValue + withdrawFieldAmount;
     withdrawBalance.innerText = withdrawBalanceTotalAmount;
+    // balance update section
+    const totalBalance = document.getElementById("balance-total");
+    const totalBalanceTextValue = totalBalance.innerText;
+    const totalBalanceValue = parseFloat(totalBalanceTextValue);
+    const totalBalanceAmount = totalBalanceValue - withdrawFieldAmount;
+    totalBalance.innerText = totalBalanceAmount;
 
+    // clear withdraw input
     withdrawField.value = "";
 
 })
